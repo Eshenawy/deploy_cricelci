782f35f262c10d65a8f10a173cbeea84e7c0a29c
Eshenawy <39653704+Eshenawy@users.noreply.github.com>
2022-11-06 19:59:19 +0200

    Update index.js

--- roles/setup/files/index.js
+++ roles/setup/files/index.js
@@ -1,6 +1,7 @@
-var http = require("http");
-var server = http.createServer(function (req, res) {
-res.writeHead(200);
-res.end("Hello world!");
-});
-server.listen(3000);
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+}).listen(3000);
